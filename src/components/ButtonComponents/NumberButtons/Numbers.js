45150e59b6cc9cfa141b830872bedbb2e2e914c4
Lucas Greenwell <lucasgreenwell15@gmail.com>
2019-12-10 12:28:42 -0600

    finsihed display and specialss

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -2,13 +2,16 @@ import React from "react";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+
+
+import {numbers} from '../../../data' 
+
+
 //Import your array data to from the provided data file
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = React.useState(numbers);  //???
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-const SpecialButton = () => {
+import data from '../../..//data'
+const SpecialButton = (props) => {
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+<button>{props.buttonName}</button>
     </>
   );
 };
+
+export default SpecialButton;
